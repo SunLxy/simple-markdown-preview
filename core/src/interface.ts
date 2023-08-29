4820a2d5f39caaba37de9a8ab0a2009157d2a246
sorryhc <49608169@qq.com>
2023-08-29 14:46:13 +0800

    fix: 修改handleMetaData 类型

--- core/src/interface.ts
+++ core/src/interface.ts
@@ -3,16 +3,21 @@ import { MarkdownPreviewProps } from '@uiw/react-markdown-preview';
 import { MdDataHandle } from './useMdData';
 import { BackToUpProps } from '@uiw/react-back-to-top';
 import type { CodeBlockData } from '@saqu/loader-md-react-preview/lib/interface';
+import { CodePreviewProps } from 'react-code-preview-layout';
+import { CodeProps } from 'react-code-preview-layout/lib/Code';
+import { PreviewProps } from 'react-code-preview-layout/lib/Preview';
+import { ToolbarProps } from 'react-code-preview-layout/lib/Toolbar';
+
 
 export interface HandleMetaDataReturn {
   /**react-code-preview-layout 组件属性*/
-  layout?: Record<string, string>
+  layout?: CodePreviewProps
   /**react-code-preview-layout Preview 组件属性*/
-  preview?: Record<string, string>
+  preview?: PreviewProps
   /**react-code-preview-layout Code 组件属性*/
-  code?: Record<string, string>
+  code?: CodeProps
   /**react-code-preview-layout Toolbar 组件属性*/
-  toolbar?: Record<string, string>
+  toolbar?: ToolbarProps
 }
 
 export interface SimplePreviewProps {
